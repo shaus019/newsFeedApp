9998e0b7e53f6db3d49650fa58d0dfd45f52e874
Usman Shah <shaus019@student.otago.ac.nz>
2020-07-20 13:31:48 +1200

    Have setupthe navigation panel.

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticleNewsFragment.kt
@@ -0,0 +1,12 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+import kotlinx.android.synthetic.main.fragment_breaking_news.view.*
+
+/**
+ * This fragment class is for article news, that will show our articles in a web view
+ * it inherits Fragment and takes the layout as a constructor paraemeter.
+ */
+class ArticleNewsFragment:Fragment(R.layout.fragment_article) {
+}
