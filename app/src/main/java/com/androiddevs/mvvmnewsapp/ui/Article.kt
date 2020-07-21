80f8c2921bd0e6dbaf3061d923a9d1d2c5ed8b85
Usman Shah <shaus019@student.otago.ac.nz>
2020-07-21 18:36:05 +1200

    Added DAO interface and adjusted Article class for saving articles in our db

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/Article.kt
@@ -1,6 +1,20 @@
 package com.androiddevs.mvvmnewsapp.ui
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * entity will tell android studio that article class is a table in our database.
+ * inside there we will need to give our table a name and the pirimary key to uniquely identify articles.
+ * The primary key will be generated automatically and nullable.
+ */
+@Entity(
+    tableName = "articles"
+
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: Any,
     val content: String,
     val description: String,
